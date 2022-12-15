3692f0f7e8e419da8982a21c02d50fcbdf3fc652
DemonMaike <dmitry.malyshevv@ya.ru>
2022-12-15 04:11:47 +0400

    Created project and add apps Person_ortfolio and Blog, create table Project

---
+++ portfolio/person_portfolio/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    photo = models.ImageField(upload_to='person_portfolio/image_portfolio')
+    url = models.URLField(blank=True)
+    
+    
